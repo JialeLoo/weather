6fe624038df49a9bd01c68830807bcda6fa3865b
EC2 Default User <ec2-user@ip-172-31-71-149.ec2.internal>
2024-04-30 02:34:46 +0000

    modifying stack to use external lib

--- weather/weather_stack.py
+++ weather/weather_stack.py
@@ -21,6 +21,9 @@ class WeatherStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        
+        # Create an S3 bucket to store weather data
+        weather_bucket = s3.Bucket(self, "WeatherAPIthedatafoundry")
 
         initiator = _alambda.PythonFunction(
             self,
@@ -28,47 +31,40 @@ class WeatherStack(Stack):
             entry='./lambda/',
             runtime=lambda_.Runtime.PYTHON_3_10,
             index='lambda_handler.py',
-            handler='lambda_handler')
+            handler='lambda_handler',
+            code=lambda_.Code.from_asset('lambda'),
+            timeout=Duration.seconds(60),
+            environment={
+                "S3_BUCKET_NAME": weather_bucket.bucket_name,
+                "base_url" : 'http://api.openweathermap.org/data/2.5/weather?',
+                "api_key" : '477860eae3ff372319537e33d0503b9b',
+                "city" : 'Melbourne,au'
+            })
             
-        # Create an S3 bucket to store weather data
-        weather_bucket = s3.Bucket(self, "WeatherAPIthedatafoundry")
-
         
-        weather_lambda = lambda_.Function(self, 'WeatherAPI',
-                                          handler='lambda_handler.lambda_handler',
-                                          runtime=lambda_.Runtime.PYTHON_3_10,
-                                          code=lambda_.Code.from_asset('lambda'),
-                                          timeout=Duration.seconds(60),
-                                          environment={
-                                            "S3_BUCKET_NAME": weather_bucket.bucket_name,
-                                            "base_url" : 'http://api.openweathermap.org/data/2.5/weather?',
-                                            "api_key" : '477860eae3ff372319537e33d0503b9b',
-                                            "city" : 'Melbourne,au'
-                                          }
-                                          )
         
         # Grant Lambda permission to write to the S3 bucket
-        weather_bucket.grant_write(weather_lambda)
+        weather_bucket.grant_write(initiator)
         
-        # # Grant permissions to the CDK stack to make changes to EventBridge rules
-        # weather_lambda.add_to_role_policy(
-        #     statement=iam.PolicyStatement(
-        #         actions=[
-        #             'events:PutRule',
-        #             'events:PutTargets',
-        #             'events:RemoveTargets',
-        #             'events:DeleteRule'
-        #         ],
-        #         resources=['*']  # Adjust the resource ARN as needed
-        #     )
-        # )
+        # Grant permissions to the CDK stack to make changes to EventBridge rules
+        initiator.add_to_role_policy(
+            statement=iam.PolicyStatement(
+                actions=[
+                    'events:PutRule',
+                    'events:PutTargets',
+                    'events:RemoveTargets',
+                    'events:DeleteRule'
+                ],
+                resources=['*']  # Adjust the resource ARN as needed
+            )
+        )
 
 
         # Create an EventBridge rule to trigger Lambda hourly
         rule = events.Rule(self, "WeatherDataRule",
             schedule=events.Schedule.cron(minute="0"),  # Trigger hourly
         )
-        rule.add_target(targets.LambdaFunction(weather_lambda))
+        rule.add_target(targets.LambdaFunction(initiator))
         
         
         
